27fde60cceb620e2535def84bab7b7c0a70cf7bc
sandeep-reddy8899 <127647369+sandeep-reddy8899@users.noreply.github.com>
2023-03-30 20:22:46 +0530

    Update docker_script.sh

--- docker_script.sh
+++ docker_script.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-R="/e[32m"
-Y="/e[31m"
-G="/e[33m"
-N="/e[0m"
+R="\e[32m"
+Y="\e[31m"
+G="\e[33m"
+N="\e[0m"
 
 LOG=docker-install.log
 USER_ID=$(id -u)
 if [ $USER_ID -ne 0 ]; then
-        echo -e "$R You are not the rrot user, you don'thave the permissions to run this $N"
+        echo -e "$R You are not the root user, you don'thave the permissions to run this $N"
         exit 1
 fi
-validate(){
+VALIDATE(){
     if [ $1 -ne 0 ]; then
         echo -e "$2.... $R FAILURE $N"
         exit 1 
     else
-        echo "$2... $G SUCESS ra abbai $N" 
+        echo -e "$2... $G SUCESS ra abbai $N" 
     fi
 }
 
 yum-update -y &>>LOG
-validate $? " Updated packages"
+VALIDATE $? " Updated packages"
