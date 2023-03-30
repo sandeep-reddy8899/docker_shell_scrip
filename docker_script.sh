65577eda75339bcd291f41108e705b1b8851f53b
sandeep-reddy8899 <127647369+sandeep-reddy8899@users.noreply.github.com>
2023-03-30 20:42:22 +0530

    Update docker_script.sh

--- docker_script.sh
+++ docker_script.sh
@@ -28,3 +28,20 @@ VALIDATE $? "Docker insatlled"
 
 service docker start &>>$LOG
 VALIDATE $? "Stated docker"
+
+systemctl enable docker &>>$LOG
+VALIDATE $? "Enabling Docker"
+
+usermod -a -G docker ec2-user &>>$LOG
+VALIDATE $? "Added ec2-user to docker group"
+
+yum install git -y &>>$LOG
+VALIDATE $? "Installing GIT"
+
+curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose &>>$LOG
+VALIDATE $? "Downloaded docker-compose"
+
+chmod +x /usr/local/bin/docker-compose
+VALIDATE $? "Moved docker-compose to local bin"
+
+echo  -e "$R You need logout and login to the server $N"
